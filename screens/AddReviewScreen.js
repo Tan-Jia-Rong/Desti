054a648138e471d453626eee4a94df9d8c39bcab
ekweirui <ekweirui@gmail.com>
2022-06-01 19:40:08 +0800

    Added the upload photos from camera and media gallery functionality

--- screens/AddReviewScreen.js
+++ screens/AddReviewScreen.js
@@ -1,22 +1,76 @@
-import { useContext, useState } from "react"
-import { Text, View, StyleSheet, Image, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Dimensions } from "react-native"
-import { FormButton, ReviewInput } from "../components"
+import { useState, useEffect } from "react"
+import { Text, View, StyleSheet, Image, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Dimensions } from "react-native";
 import { AuthContext } from "../navigation/AuthProvider"
-import { InputWrapper, InputField } from '../styles/AddReview';
+import { InputWrapper, InputField, AddImage, StatusWrapper, SubmitBtn, SubmitBtnText } from '../styles/AddReview';
 import ActionButton from "react-native-action-button";
 import Icon from 'react-native-vector-icons/Ionicons';
-import { CurrentRenderContext } from "@react-navigation/native";
+import * as ImagePicker from "expo-image-picker";
 
 
 const AddReviewScreen = ({navigation}) => {
-  const {user} = useContext(AuthContext);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [image, setImage] = useState(null);
   const [review, setReview] = useState("");
+
+  // Ask for permissions to user's camera and media gallery
+  useEffect(() => {
+    (async () => {
+      const camerastatus = await ImagePicker.requestCameraPermissionsAsync();
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasCameraPermission(camerastatus.status === 'granted');
+      setHasGalleryPermission(galleryStatus.status === 'granted');
+    })();
+  }, []);
+
+  // Function for taking photo from camera
+  const takePhotoFromCamera = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    // If result is not cancelled
+    if (!result.cancelled) {
+      setImage(result.uri);
+    };
+  }
+
+  // Function for choosing photo from media library
+  const choosePhotoFromLibrary = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    // If result is not cancelled
+    if (!result.cancelled) {
+      setImage(result.uri);
+    };
+  }
+  
+  if (hasCameraPermission === false && hasGalleryPermission === false) {
+    return <Text>No permission access to camera and photo gallery</Text>
+  }
+
+  if (hasCameraPermission === false ) {
+    return <Text>No permission access to camera</Text>
+  }
+
+  if (hasGalleryPermission === false) {
+    return <Text>No permission access to photo gallery</Text>
+  }
 
   return (
     <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={{flex: 1}}>
       <InputWrapper>
+        {image === null ? <AddImage source={{uri: 'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png'}} /> : <AddImage source={{uri: image}} />}
         <InputField 
           value ={review}
           onChangeText={setReview} 
@@ -29,13 +83,13 @@ const AddReviewScreen = ({navigation}) => {
         <ActionButton.Item
           buttonColor="#9b59b6"
           title="Take Photo"
-          onPress={() => {}}>
+          onPress={takePhotoFromCamera}>
           <Icon name="camera-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
         <ActionButton.Item
           buttonColor="#3498db"
           title="Choose Photo"
-          onPress={() => {}}>
+          onPress={choosePhotoFromLibrary}>
           <Icon name="md-images-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
       </ActionButton>
