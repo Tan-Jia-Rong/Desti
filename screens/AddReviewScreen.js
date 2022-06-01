d6218255491c079e80aa3581ab9b2619af156a93
ekweirui <ekweirui@gmail.com>
2022-06-02 04:52:09 +0800

    Added uploading indicator loader

--- screens/AddReviewScreen.js
+++ screens/AddReviewScreen.js
@@ -1,12 +1,12 @@
 import { useState, useEffect, useContext } from "react"
-import { Text, View, StyleSheet, Image, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Dimensions, Alert } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, Dimensions, Alert, ActivityIndicator } from "react-native";
 import { AuthContext } from "../navigation/AuthProvider"
 import { InputWrapper, InputField, AddImage, StatusWrapper, SubmitBtn, SubmitBtnText } from '../styles/AddReview';
 import ActionButton from "react-native-action-button";
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as ImagePicker from "expo-image-picker";
 import { storage } from "../firebase";
-import { ref, uploadBytes } from "firebase/storage";
+import { ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
 
 
 const AddReviewScreen = ({navigation}) => {
@@ -16,10 +16,11 @@ const AddReviewScreen = ({navigation}) => {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
   const [image, setImage] = useState(null);
   const [uploading, setUpLoading] = useState(false);
+  // Transferred bytes
   const [transferred, setTransferred] = useState(0);
   const [review, setReview] = useState("");
 
-  // Ask for permissions to user's camera and media gallery
+  // Ask for permissions to access user's camera and media gallery
   useEffect(() => {
     (async () => {
       const camerastatus = await ImagePicker.requestCameraPermissionsAsync();
@@ -67,9 +68,15 @@ const AddReviewScreen = ({navigation}) => {
     const uploadUri = image;
     // Get the last part of the string
     let fileName = uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
+
+    // Add timestamp to fileName to give unique fileNames, ie string manipulation
+    // Split method splits a string into an array of substrings, then pop it
+    const extension = fileName.split('.').pop();
+    const name = fileName.split('.').slice(0, -1).join('.');
+    fileName = name + Date.now() + '.' + extension;
     
     setUpLoading(true);
-    Alert.alert('Image uploaded to the Firebase Cloud Storage successfully');
+    setTransferred(0);
 
     try {
       // A reference is a local pointer to some file on your bucket. This
@@ -81,12 +88,29 @@ const AddReviewScreen = ({navigation}) => {
       const bytes = await img.blob();
 
       // Upload the image
-      await uploadBytes(reference, bytes);
+      const uploadTask = uploadBytesResumable(reference, bytes);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes * 100);
+          console.log('Upload is ' + progress + '% done');
+
+          setTransferred(Math.round(snapshot.bytesTransferred / snapshot.totalBytes * 100));
+        }
+      );
+      Alert.alert('Image uploaded to the Firebase Cloud Storage successfully');
     } catch (e) {
       console.log(e);
     }
 
     // After post has been uploaded to the Firebase Cloud Storage
+    setUpLoading(false);
     setImage(null);
   }
   
@@ -117,9 +141,16 @@ const AddReviewScreen = ({navigation}) => {
           multiLine={true}
           numberOfLines={4}
           />
-          <SubmitBtn onPress={submitPost}>
-            <SubmitBtnText>Post</SubmitBtnText>
-          </SubmitBtn>
+          {uploading ? (
+            <StatusWrapper>
+              <Text>{transferred}% Completed</Text>
+              <ActivityIndicator size='large' color='#0000ff' />
+            </StatusWrapper>
+          ) : (
+            <SubmitBtn onPress={submitPost}>
+              <SubmitBtnText>Post</SubmitBtnText>
+            </SubmitBtn>
+          )}
       </InputWrapper>
 
       <ActionButton buttonColor="#2e64e5">
